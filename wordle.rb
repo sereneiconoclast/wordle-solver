1de7c046b6f8941f0af2b5b6451f485cf353da14
Greg Meyers <greg.meyers.1138@gmail.com>
2025-07-21 10:08:00 +0000

    Make some notes about ways wordle.rb could be improved

--- wordle.rb
+++ wordle.rb
@@ -1,10 +1,42 @@
 #!/home/ec2-user/.asdf/shims/ruby
+# Make a guess in Wordle (start with 'crane') and note the results.
+# Run this program with the word, then a dash, then five characters
+# describing the outcome: . for gray (not present), y for yellow
+# (present but not in this position), g for green (letter is in right
+# position).
+#
+# Then make another guess, rerun the program and add another word and its
+# outcome to narrow the field of possibilities further.
 #
 # For now, with a double letter where one is yellow and the other gray,
-# enter yellow as the outcome for both
+# enter yellow as the outcome for both. If one is green and the other gray,
+# enter green and yellow.
 #
 # Example usage:
 # ./wordle.rb crane-..... sloth-..y.. opium-y.g.g (answer is "idiom")
+#
+# TODO, part 1: Keep track of how many occurrences of each letter are
+# possible, e.g.:
+# {
+#   'a' => (0..3), # I don't think any word has more than 3 of the same letter
+#   'b' => (0..3),
+#   'c' => (0..3),
+#   ...
+# }
+#
+# When a gray letter shows up, set the range to (0..0).
+# More useful: Use this whenever one or more yellow letters show up.
+# Examine the outcome to control for how many of the letter might be there.
+# If there's only one occurrence and it's yellow, zero is eliminated so
+# change the range to (1..3).
+# If there are two occurrences, one yellow and one gray, then the range is
+# just (1..1) - this is also the case if one is green and one gray.
+# If there are two and they're both yellow (or green), the range is (2..3).
+# If there are three and two are yellow (or green), it's (2..2).
+# If there are three and they're all yellow... well that's impossible.
+#
+# TODO, part 2: Examine the possible dictionary matches after filtering,
+# and do the bucketing analysis used by NYTimes's WordleBot.
 alphabet = (97..122).map(&:chr)
 $available = (0..4).map { alphabet.dup }
 
@@ -18,8 +50,9 @@ def go(word, outcome)
     case o
     when '.' # Gray - letter appears in none of the 5 positions
       # TODO: Deal with double letter having one gray outcome, one yellow
-      # Needs to record that exactly one is present, but not in either position,
-      # and only once in the word (so it needs to count occurrences)
+      # This "completely rule out the letter" logic only works when every
+      # occurrence of the letter in the guess is gray (which is usually
+      # because the letter only occurs once)
       $available.each { |av2| av2.delete(w) }
     when 'g' # Green - letter is definitely in this position
       av.clear
